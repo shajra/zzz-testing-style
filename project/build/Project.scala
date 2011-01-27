c48d6e2e7750c2f155f45778237d300617c33647
Sukant Hajra <rrudbskr5g@snkmail.com>
2011-01-26 21:35:15 -0600

    updated some versions for the lastest Scala releases

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -3,9 +3,11 @@ import sbt._
 
 class Project(info: ProjectInfo) extends DefaultProject(info) with IdeaProject {
 
-  val specs = "org.scala-tools.testing" %% "specs" % "1.6.5" % "test" withSources ()
+  val specs = "org.scala-tools.testing" %% "specs" % "1.6.7" % "test" withSources()
+
   val junit = "junit" % "junit" % "4.8.1" % "test"
-  val junitInterface = "com.novocode" % "junit-interface" % "0.4" % "test"
+
+  val junitInterface = "com.novocode" % "junit-interface" % "0.5" % "test"
 
   // override def defaultLoggingLevel = Level.Debug
 
